cf9e4046be06a1320851e2c925e39ecb727a3293
troy dorman <troydorman@gmail.com>
2020-12-18 19:50:20 -0700

    working on api routes

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ let fs = require('fs');
 
 let app = express();
 
+let db = require('./db/db.json');
+
+
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing | middlewear
@@ -28,46 +31,48 @@ app.get("/notes", function (req, res) {
 
 // api routes
 
-app.get("/api/notes", function (req, res) {
-    // we need to return the contents of db.json 
-    let databaseFile = fs.readFile("./db/db.json", (err, data) => {
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, notesData) => {
         if (err) throw err;
-        console.log(data);
-        return res.json(data);
-
-
+        res.json(JSON.parse(notesData));
     });
-
-
 });
 
 
-/* 
-app.post("/api/notes", function (req, res) {
+app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        let notesData = JSON.parse(data);
+        notesData.push(req.body);
+        console.log(req.body);
+        for (let i = 0; i < notesData.length; i++) {
+            notesData[i].id = i + 1;
+        }
+        fs.writeFile("./db/db.json", JSON.stringify(notesData), (err) => {
+            if (err) throw err;
+            res.send(db);
+        });
+    });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        let newNote = JSON.parse(data);
+        for (let i = 0; i < newNote.length; i++) {
+            if (newNote[i].id == req.params.id) {
+                newNote.splice(i, 1);
+            }
+        }
+
+        fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
+            if (err) throw err;
+            res.send(newNote);
+        });
+    });
+});
 
 
-
-
-}); */
-
-
-
-
-/*
-The following API routes should be created:
-
-
-GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-
-
-POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-
-
-DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-
-
-
-*/
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
